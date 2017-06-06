8230cad9f7fb642aa4213e1ea6ac73000df2c22d
Siviwe Xakaza <xakazampendulo@gmail.com>
2017-06-06 13:30:10 +0200

    Add files via upload

---
+++ Controllers/APIController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void addPerson(Person p)
+        {
+            if (p != null)
+            {
+                db.people.Add(p);
+                db.SaveChanges();
+            }
+            
+        }
+
+        public JsonResult getPeople()
+        {
+            var people = db.people.ToList();
+            return Json(people, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Controllers/PagesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PagesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult People()
+        {
+            return View();
+        }
+
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if(id != null)
+            {
+                var person = db.people.ToList().Where(x => x.ID == id).First();
+                ViewBag.Person = person.Name + " " + person.Surname;
+            }
+            return View();
+        }
+    }
+}
